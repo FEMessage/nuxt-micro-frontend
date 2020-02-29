4d209f84b743f27b479bc37c3e15915ec6dafc8d
Han <xsytby1112@gmail.com>
2020-02-29 16:36:29 +0800

    ci: fix repo info

--- notify.sh
+++ notify.sh
@@ -5,12 +5,12 @@ then
   exit 1
 fi
 
-git remote add github https://$GITHUB_TOKEN@github.com/{{ownerName}}/{{componentName}}.git > /dev/null 2>&1
+git remote add github https://$GITHUB_TOKEN@github.com/FEMessage/nuxt-micro-frontend.git > /dev/null 2>&1
 git push github HEAD:master --follow-tags
 
 GREN_GITHUB_TOKEN=$GITHUB_TOKEN yarn release
 
-url=https://api.github.com/repos/{{ownerName}}/{{componentName}}/releases/latest
+url=https://api.github.com/repos/FEMessage/nuxt-micro-frontend/releases/latest
 resp_tmp_file=resp.tmp
 
 curl -H "Authorization: token $GITHUB_TOKEN" $url > $resp_tmp_file
@@ -19,7 +19,7 @@ html_url=$(sed -n 5p $resp_tmp_file | sed 's/\"html_url\"://g' | awk -F '"' '{pr
 body=$(grep body < $resp_tmp_file | sed 's/\"body\"://g;s/\"//g')
 version=$(echo $html_url | awk -F '/' '{print $NF}')
 
-msg='{"msgtype": "markdown", "markdown": {"title": "{{componentName}}更新", "text": "@所有人\n# [{{componentName}}('$version')]('$html_url')\n'$body'"}}'
+msg='{"msgtype": "markdown", "markdown": {"title": "nuxt-micro-frontend更新", "text": "@所有人\n# [nuxt-micro-frontend('$version')]('$html_url')\n'$body'"}}'
 
 curl -X POST https://oapi.dingtalk.com/robot/send\?access_token\=$DINGTALK_ROBOT_TOKEN -H 'Content-Type: application/json' -d "$msg"
 
