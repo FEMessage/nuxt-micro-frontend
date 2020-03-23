d290a1eb8edb447c94feed44e7d0f9dbd13017c5
Deepen Lau <lpd09009@gmail.com>
2020-03-23 18:52:57 +0800

    feat: dev-server-cors

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -2,8 +2,25 @@ const { resolve } = require('path')
 
 module.exports = {
   mode: 'spa',
+  build: {
+    devMiddleware: {
+      // 这个 headers 是静态资源文件的路由，这里是 /_nuxt
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
+  },
   MFE: {
-    path: 'example/mfe.js'
+    force: true,
+    path: 'example/mfe.js',
+    devServer: {
+      // 写自己服务的绝对路径，斜杠结尾，必填
+      publicPath: '//localhost:3000/',
+      // 这个 headers 会通过 addServerMiddleware 注入
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
   },
   rootDir: resolve(__dirname, '..'),
   buildDir: resolve(__dirname, '.nuxt'),
