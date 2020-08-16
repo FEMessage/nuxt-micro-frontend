210987c55565f8b6a39d8481bacb9d539f720e93
cjfff <cjfff1996@gmali.com>
2020-08-16 22:22:23 +0800

    chore: dir change name

---
+++ examples/nuxt-main-self-load-demo/nuxt-qiankun-project/pages/404.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div class="page-404">
+    page 404
+    <button @click="$router.replace('/')">返回首页</button>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'empty'
+}
+</script>
+
+<style>
+.page-404 {
+  text-align: center;
+}
+</style>
---
+++ examples/nuxt-main-self-load-demo/nuxt-qiankun-project/pages/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    home page
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'main'
+}
+</script>
+
+<style>
+
+</style>
